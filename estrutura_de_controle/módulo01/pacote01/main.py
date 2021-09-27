ee42484556f244bf6aa2746c1e15eb90a7f14913
pedro-hdias <83011148+pedro-hdias@users.noreply.github.com>
2021-09-27 07:47:31 -0300

    todos os arquivos do exercício adicionados.

--- estrutura_de_controle/módulo01/pacote01/main.py
+++ estrutura_de_controle/módulo01/pacote01/main.py
@@ -2,6 +2,14 @@ def leiaInt(msg):
 	while True:
 		try:
 			n = int(input(msg))
+		except	(ValueError, TypeError):
+			print("ERRO: Por favor, digite um número inteiro válido.")
+			continue
+		except(KeyboardInterrupt):
+			print("Usuário decidiu não digitar este número: ")
+			break
+		else:
+			return n
 
-num = int(input("Digite um número inteiro: "))
-print(f"O valor digitado foi: {num}")
+num = leiaInt("Digite um número inteiro: ")
+print(f"O valor digitado foi: {num}.")
